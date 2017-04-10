e4344e2a6ba0d82867211e1b7f69f9bde5b88b7f
Edu095 <eduojerb@gmail.com>
2017-04-10 14:00:59 +0200

    [IMP] web view courses

--- __manifest__.py
+++ __manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Pruebas',
-    'version': '0.5',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'board', 'website', 'mail', 'website_sale'],

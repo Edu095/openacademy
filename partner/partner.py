5c523e152a7f162988ee19151206914256963768
Edu095 <eduojerb@gmail.com>
2017-05-04 13:41:37 +0200

    [IMP]

--- partner/partner.py
+++ partner/partner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import fields, models
 
+
 class Partner(models.Model):
     _inherit = 'res.partner'
 
@@ -9,4 +10,4 @@ class Partner(models.Model):
     instructor = fields.Boolean("Instructor", default=False)
 
     session_ids = fields.Many2many('openacademy.session',
-        string="Attended Sessions", readonly=True)
+                                   string="Attended Sessions", readonly=True)
